858ee94c9ab1a8282f80c5b532c24f6d2cc183cb
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-03 21:56:28 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+
+import CustomLink from '../CustomLink/CustomLink';
+ 
+
 
 
 const Header = () => {
     return (
-        <div className='flex justify-around items-center p-4'>
+        <div className='flex justify-around items-center p-4 shadow-lg '>
             <div className='text-4xl font-medium text-orange-600'>
                 <h1>LogoSection</h1>
             </div>
-            <div className='text-xl'>
-                <nav className=''>
-                    <NavLink className={({ isActive }) =>
-                        isActive ? "text-orange-600" : "text-orange-700"
-                    } to='/'>Home</NavLink>
-                    <NavLink className={({ isActive }) =>
-                        isActive ? "text-orange-600" : "text-orange-700"
-                    } to='/food-menu'>Menu</NavLink>
-                    <NavLink className={({ isActive }) =>
-                        isActive ? "text-orange-600" : "text-orange-700"
-                    } to='/foods'>Foods</NavLink>
-                    <NavLink className={({ isActive }) =>
-                        isActive ? "text-orange-600" : "text-orange-700"
-                    } to='/orders'>Orders</NavLink>
-                </nav>
+            <div className='md:flex justify-center items-center text-xl'>       
+                <CustomLink className='mr-7' to='/'>Home</CustomLink>
+                <CustomLink className='mr-7' to='/food-menu'>Food Menu</CustomLink>
+                <CustomLink className='mr-7' to='/orders'>Orders</CustomLink>
+                <CustomLink className='mr-7' to='/shop'>Shop</CustomLink>
+                <CustomLink className='mr-7' to='/blog'>Blog</CustomLink>
             </div>
-            <div className='text-orange-600 '>
-                <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+            <div className='text-orange-600'>
+                <FontAwesomeIcon className='mr-4 bg-orange-100 p-3 rounded-full' icon={faUser}></FontAwesomeIcon>
+                <FontAwesomeIcon className='mr-4 bg-orange-100 p-3 rounded-full' icon={faCartShopping}></FontAwesomeIcon>
+                <FontAwesomeIcon className='mr-4 bg-orange-100 p-3 rounded-full' icon={faSearch}></FontAwesomeIcon>
             </div>
         </div>
     );
